e9e2b4b06b26cf7d3324829f0c4690eca0cbd0a9
Marco Clemencic <marco.clemencic@cern.ch>
2016-05-24 11:01:11 +0200

    take into account the "disabled" flag of projects

--- gen_make_definitions.py
+++ gen_make_definitions.py
@@ -4,11 +4,12 @@ from Future import lhcb_upgrade as slot
 
 with open(sys.argv[1], 'w') as makefile:
     makefile.write('PROJECTS = {0}\n'
-                   .format(' '.join(p.name for p in slot.projects)))
+                   .format(' '.join(p.name for p in slot.activeProjects)))
     makefile.write('PROJECTS_UPCASE = {0}\n'
-                   .format(' '.join(p.name.upper() for p in slot.projects)))
+                   .format(' '.join(p.name.upper()
+                                    for p in slot.activeProjects)))
     makefile.write('PROJECTS_DIRS = {0}\n'
-                   .format(' '.join(p.baseDir for p in slot.projects)))
+                   .format(' '.join(p.baseDir for p in slot.activeProjects)))
 
     for p in slot.projects:
         makefile.write('{0}_DIR = {1}\n'
--- gen_make_deps.py
+++ gen_make_deps.py
@@ -3,8 +3,8 @@ import sys
 from Future import lhcb_upgrade as slot
 
 with open(sys.argv[1], 'w') as makefile:
-    names = set(p.name for p in slot.projects)
-    for p in slot.projects:
+    names = set(p.name for p in slot.activeProjects)
+    for p in slot.activeProjects:
         deps = names.intersection(p.dependencies())
         makefile.write('{0}: {1}\n'
                        .format(p.name, ' '.join(deps)))
