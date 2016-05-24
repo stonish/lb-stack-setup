993d2c312280782e48f0e25f88689f562b3e06e1
Marco Clemencic <marco.clemencic@cern.ch>
2016-05-24 12:44:21 +0200

    ensure that checkout does update the sources

--- checkout.py
+++ checkout.py
@@ -1,9 +1,14 @@
 import os
 import logging
 from Future import lhcb_upgrade
+from subprocess import call
 
 logging.basicConfig(level=logging.DEBUG if os.environ.get('VERBOSE')
                     else logging.INFO)
 
+# update the clones (not done by the checkout method of the slot)
+for p in lhcb_upgrade.projects:
+    if os.path.exists(os.path.join(p.baseDir, '.git')):
+        call(['git', 'fetch', 'origin'], cwd=p.baseDir)
 lhcb_upgrade.checkout()
 lhcb_upgrade.patch()
