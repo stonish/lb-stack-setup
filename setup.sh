08a4e581e8e0ad4e7eff9630eb1328d48af1ec30
Marco Clemencic <marco.clemencic@cern.ch>
2016-11-11 09:40:52 +0100

    use LCG 86 from /cvmfs/sft.cern.ch

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 export CCACHE_DIR=${PWD}/.ccache
 export CMAKEFLAGS=-DCMAKE_USE_CCACHE=ON
-export CMAKE_PREFIX_PATH=${PWD}:${CMAKE_PREFIX_PATH}
+export CMAKE_PREFIX_PATH=${PWD}:/cvmfs/sft.cern.ch/lcg/releases:${CMAKE_PREFIX_PATH}
 export VERBOSE=
 export PATH=${PATH}:/cvmfs/lhcb.cern.ch/lib/contrib/ninja/1.4.0/x86_64-slc6
