b28e920ac635a39b3d6e187ec575e052996bcf79
Rosen Matev <rosen.matev@cern.ch>
2017-02-05 23:13:29 +0100

    Do not build the functor caches by default

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export CCACHE_DIR=${PWD}/.ccache
 #export CCACHE_LOGFILE=$(dirname $(mktemp -u))/ccache.debug
-export CMAKEFLAGS=-DCMAKE_USE_CCACHE=ON
+export CMAKEFLAGS="-DCMAKE_USE_CCACHE=ON -DLOKI_BUILD_FUNCTOR_CACHE=FALSE"
 export CMAKE_PREFIX_PATH=${PWD}:${CMAKE_PREFIX_PATH}
 export VERBOSE=
 export PATH=${PATH}:/cvmfs/lhcb.cern.ch/lib/contrib/ninja/1.4.0/x86_64-slc6
