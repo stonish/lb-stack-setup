5ccd34f9ac8d1831bc1bfb9999194509d806d8e1
Rosen Matev <rosen.matev@cern.ch>
2018-02-21 18:20:04 +0100

    Compile the preprocessed source on ccache miss

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ set -e
 export CCACHE_DIR=${PWD}/.ccache
 export CCACHE_TEMPDIR=${XDG_RUNTIME_DIR:-$(dirname $(mktemp -u))/$(id -u)}/ccache-tmp  # use a faster TMPDIR
 mkdir -p ${CCACHE_TEMPDIR}
+export CCACHE_NOCPP2=1  # compile the preprocessed source on cache miss; faster but possibly different diagnostic warnings
 #export CCACHE_LOGFILE=$(dirname $(mktemp -u))/ccache.debug
 export CMAKEFLAGS="-DCMAKE_USE_CCACHE=ON -DLOKI_BUILD_FUNCTOR_CACHE=FALSE --no-warn-unused-cli"
 export CMAKE_PREFIX_PATH=${PWD}:${CMAKE_PREFIX_PATH}
