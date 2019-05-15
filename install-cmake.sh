844911cdcf3c143ce5dc1ac41a052a93fd49a237
Rosen Matev <rosen.matev@cern.ch>
2019-05-15 18:59:56 +0200

    Fix install

--- install-cmake.sh
+++ install-cmake.sh
@@ -4,8 +4,7 @@
 
 setup 'https://gitlab.kitware.com/rmatev/cmake.git' 'master-5a2023f904-lhcb'
 (
-    . /cvmfs/sft.cern.ch/lcg/views/LCG_95/x86_64-centos7-gcc8-opt/setup.sh
-    cmake -DCMAKE_INSTALL_PREFIX:PATH=$CONTRIB .
+    cmake "-DCMAKE_INSTALL_PREFIX:PATH=$CONTRIB" .
     make install -j$(nproc)
 )
 cleanup
--- install-ninja.sh
+++ install-ninja.sh
@@ -5,7 +5,6 @@
 # master on 10 May 2019
 setup 'https://github.com/ninja-build/ninja.git' '20b30dac6698d119e7797b34d6ed2c4ed8f48417'
 (
-    . /cvmfs/sft.cern.ch/lcg/views/LCG_95/x86_64-centos7-gcc8-opt/setup.sh
     ./configure.py --bootstrap
     cp ninja "$CONTRIB/bin"
 )
