eba2fecc3e612ac5c30494042a5ccba262bfe203
Rosen Matev <rosen.matev@cern.ch>
2019-05-20 17:07:18 +0200

    Move BINARY_TAG setting in config and fix non-docker

--- install.sh
+++ install.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
+set -e
 
 DIR=$(dirname $0)
 
-utils/build-env bash -c "
-bash "$DIR/install-cmake.sh"
-bash "$DIR/install-ninja.sh"
-bash "$DIR/install-ccache.sh"
-bash "$DIR/install-distcc.sh"
+"${DIR}/build-env" bash -c "
+bash '$DIR/install-cmake.sh'
+bash '$DIR/install-ninja.sh'
+bash '$DIR/install-ccache.sh'
+bash '$DIR/install-distcc.sh'
 "
 
-bash $DIR/install-tools.sh
+bash "$DIR/install-tools.sh"
+
+# test that everything was installed ok
+ls cmake ninja ccache distcc pump ninjatracing post_build_ninja_summary.py
