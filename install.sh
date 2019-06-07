519133196a4765f2be02e0dfbd0ffa5d79a087a5
Rosen Matev <rosen.matev@cern.ch>
2019-06-07 19:48:53 +0200

    Fix check that everything is installed

--- install.sh
+++ install.sh
@@ -13,4 +13,4 @@ bash '$DIR/install-distcc.sh'
 bash "$DIR/install-tools.sh"
 
 # test that everything was installed ok
-ls cmake ninja ccache distcc pump ninjatracing post_build_ninja_summary.py
+ls contrib/bin/{cmake,ninja,ccache,distcc,pump,ninjatracing,post_build_ninja_summary.py} >/dev/null
