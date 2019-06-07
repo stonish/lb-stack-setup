d9ceb46a5fbb6f18de28a624a563d3187eb086cf
Rosen Matev <rosen.matev@cern.ch>
2019-06-07 19:47:48 +0200

    Fix github checkout

--- install-common.sh
+++ install-common.sh
@@ -15,16 +15,15 @@ setup() {
         mkdir -p "$SRC"
         
         cd "$SRC"
-        # "shallow clone" working with a sha:
         git init
         git remote add origin "$REPO"
-        git fetch --depth 1 origin "$SHA"
-        git checkout FETCH_HEAD
-        # shallow clone
-        # git clone -b "$SHA" --single-branch --depth 1 "$REPO" "$SRC"
-        # full clone with checkout
-        # git clone "$REPO" "$SRC"
-        # git checkout "$SHA"
+        # try fetching only the commit, if remote does not allow, do full fetch
+        if git fetch --depth 1 origin "$SHA" 2>/dev/null; then
+            git checkout FETCH_HEAD
+        else
+            git fetch origin
+            git checkout "$SHA"
+        fi
     fi
 
     if [ "$REMOVE" = true ]; then
