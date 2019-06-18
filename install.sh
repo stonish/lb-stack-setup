1c426ad627329b2448ed3dd79c51f698d181b856
Rosen Matev <rosen.matev@cern.ch>
2019-06-18 21:48:36 +0200

    Run all installs in build env

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ bash '$DIR/install-ccache.sh'
 bash '$DIR/install-distcc.sh'
 "
 
-bash "$DIR/install-tools.sh"
+"${DIR}/build-env" bash "$DIR/install-tools.sh"
 
 # test that everything was installed ok
 ls contrib/bin/{cmake,ninja,ccache,distcc,pump,ninjatracing,post_build_ninja_summary.py} >/dev/null
