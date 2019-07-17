06d7f773682a4a829730f4d5d5e9a9ec0373ed15
Rosen Matev <rosen.matev@cern.ch>
2019-07-17 16:40:23 +0200

    Simpler git checkout and use logging

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ if sys.version_info < (2, 7):
     sys.exit("Python 2.7 or later is required.")
 
 import argparse
+import logging
 import os
 import errno
 import platform
@@ -18,12 +19,14 @@ FROM_FILE = os.path.isfile(__file__)
 CVMFS_DIRS = [
     # (path, mandatory)
     ('/cvmfs/lhcb.cern.ch', True),
+    ('/cvmfs/lhcb-condb.cern.ch', True),
     ('/cvmfs/lhcbdev.cern.ch', False),
     ('/cvmfs/sft.cern.ch', False),
 ]
 CVMFS_GIT = '/cvmfs/lhcb.cern.ch/lib/contrib/git/2.14.2/bin/git'
 GIT = 'git'
-REPO = 'https://gitlab.cern.ch/rmatev/lb-stack-setup.git'
+URL_BASE = 'https://gitlab.cern.ch/rmatev/lb-stack-setup'
+REPO = URL_BASE + '.git'
 BRANCH = 'master'
 # TODO test that url and branch matches repo in a CI test?
 
@@ -32,21 +35,32 @@ parser.add_argument('path', help='Path to stack directory',
                     **({'nargs': '?'} if FROM_FILE else {}))
 parser.add_argument('--repo', '-u', default=REPO, help='Repository URL')
 parser.add_argument('--branch', '-b', default=BRANCH, help='Branch')
+parser.add_argument('--debug', action='store_true', help='Debugging output')
 args = parser.parse_args()
+
+logging.basicConfig(format='%(levelname)-7s %(message)s',
+                    level=(logging.DEBUG if args.debug else logging.INFO))
 
 if not args.path:
     args.path = os.path.abspath(join(os.path.dirname(__file__), '..'))
-    print('Guessed path to stack: {}'.format(args.path))
+    logging.info('Guessed path to stack: {}'.format(args.path))
 
+inaccessible_dirs = False
 for path, mandatory in CVMFS_DIRS:
     try:
         if not os.listdir(path):
-            raise RuntimeError('Directory {!r} is empty'.format(path))
+            logging.error('Directory {!r} is empty'.format(path))
+            inaccessible_dirs = True
     except (OSError, RuntimeError) as e:
         msg = 'Directory {!r} is not accessible: {!s}'.format(path, str(e))
         if not mandatory:
-            warnings.warn(msg)
-        raise RuntimeError(msg)
+            logging.warning(msg)
+        else:
+            logging.error(msg)
+            inaccessible_dirs = True
+if inaccessible_dirs:
+    sys.exit('Some needed directories are not accessible.\n'
+             'Check {}/blob/master/doc/prerequisites.md'.format(URL_BASE))
 
 # Check host OS
 host_os = (check_output('/cvmfs/lhcb.cern.ch/lib/bin/host_os')
@@ -56,21 +70,21 @@ if host_os == 'x86_64-centos7':
     # test native setup
     pass
 else:
-    print('Platform {!s} is not supported natively, checking for docker...'
-          .format(host_os))
+    logging.info('Platform {!s} is not supported natively, '
+                 'checking for docker...'.format(host_os))
     try:
         check_output(['docker', 'run', '--rm', 'hello-world'])
     except CalledProcessError as e:
         sys.exit('Docker not available or not set up correctly.')
-    print('...using docker.')
+    logging.info('...using docker.')
     use_docker = True
 
 # Check git version and suggest alias if too old
 git_ver_str = check_output(['git', '--version']).decode('ascii').strip()
 git_ver = LooseVersion(git_ver_str.split()[2])
 if git_ver < LooseVersion('2.13'):
-    print('Old unspported git version {} detected. Consider using\n'
-          '    alias git={}'.format(git_ver, CVMFS_GIT))
+    logging.warning('Old unspported git version {} detected. Consider using\n'
+                    '    alias git={}'.format(git_ver, CVMFS_GIT))
 
 stack_dir = args.path
 utils_dir = join(stack_dir, 'utils')
@@ -83,7 +97,8 @@ except OSError as e:
     if e.errno == errno.EEXIST:
         if os.listdir(stack_dir):
             update_setup = True
-            print('Updating existing stack setup in {}'.format(stack_dir))
+            logging.info('Updating existing stack setup in {}'
+                         .format(stack_dir))
     else:
         sys.exit('Could not create directory {!r}: {!s}'
                  .format(stack_dir, e.strerror))
@@ -93,14 +108,14 @@ if update_setup:
              'Please update manually with git pull in utils.')
 
 check_call([GIT, 'clone', '-q', args.repo, join(stack_dir, 'utils')])
-check_call([GIT, 'checkout', args.branch], cwd=join(stack_dir, 'utils'))
+check_call([GIT, 'checkout', '-q', args.branch], cwd=join(stack_dir, 'utils'))
 check_call([join(utils_dir, 'config.py'), 'useDocker', str(use_docker).lower()])
 # the target needs to be relative
 os.symlink(join('utils', 'Makefile'), join(stack_dir, 'Makefile'))
 
 # TODO check free space and warn?
 
-print("""
+logging.info("""
 Now do
 
     cd "{!s}"
