639aa091cb0c89c4b7df6139f8299e44663475b5
Rosen Matev <rosen.matev@cern.ch>
2019-07-19 19:14:31 +0200

    Do not require kerberos for install

--- install.sh
+++ install.sh
@@ -3,14 +3,14 @@ set -eo pipefail
 
 DIR=$(dirname $0)
 
-"${DIR}/build-env" bash -c "
+"${DIR}/build-env" --no-kerberos bash -c "
 bash '$DIR/install-cmake.sh'
 bash '$DIR/install-ninja.sh'
 bash '$DIR/install-ccache.sh'
 bash '$DIR/install-distcc.sh'
 "
 
-"${DIR}/build-env" bash "$DIR/install-tools.sh"
+"${DIR}/build-env" --no-kerberos bash "$DIR/install-tools.sh"
 
 # test that everything was installed ok
 ls contrib/bin/{cmake,ninja,ccache,distcc,pump,ninjatracing,post_build_ninja_summary.py} >/dev/null
