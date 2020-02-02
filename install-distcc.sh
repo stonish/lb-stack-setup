edae8ebdbc2e49810f7bc948442f26f620d1d763
Rosen Matev <rosen.matev@cern.ch>
2020-02-02 17:45:31 +0100

    Rebase on distcc upstream master

--- install-distcc.sh
+++ install-distcc.sh
@@ -3,9 +3,9 @@ set -eo pipefail
 
 . $(dirname $0)/install-common.sh
 
-setup 'https://github.com/rmatev/distcc.git' 'master-76d8dc6-lhcb'
+setup 'https://github.com/rmatev/distcc.git' 'master-fd943af-lhcb'
 (
-    . /cvmfs/sft.cern.ch/lcg/views/LCG_96bpython3/x86_64-centos7-gcc8-opt/setup.sh
+    . /cvmfs/sft.cern.ch/lcg/views/LCG_96bpython3/x86_64-centos7-gcc9-opt/setup.sh
     ./autogen.sh
     ./configure --prefix $CONTRIB --with-auth --without-libiberty
     make install -j$(nproc)
