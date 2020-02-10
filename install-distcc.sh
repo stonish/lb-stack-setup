8dfe4932e14c1ea04ccc9bfe7d4efc35a76a8a6e
Rosen Matev <rosen.matev@cern.ch>
2020-02-10 10:29:33 +0100

    Update distcc version (include server fix for Online)

--- install-distcc.sh
+++ install-distcc.sh
@@ -3,7 +3,7 @@ set -eo pipefail
 
 . $(dirname $0)/install-common.sh
 
-setup 'https://github.com/rmatev/distcc.git' 'master-fd943af-lhcb'
+setup 'https://github.com/rmatev/distcc.git' '4387788e198a372812da66b91de8f303ba65d940'
 (
     . /cvmfs/sft.cern.ch/lcg/views/LCG_96bpython3/x86_64-centos7-gcc9-opt/setup.sh
     ./autogen.sh
