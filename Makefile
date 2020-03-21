6f3f2b6d503653d15a2377f3fc3630eaac7d3fc9
Rosen Matev <rosen.matev@cern.ch>
2020-03-21 12:00:18 +0100

    Move checkout from Makefile into setup-make.py

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 DIR := $(abspath $(dir $(realpath $(lastword $(MAKEFILE_LIST)))))
 
-# project settings
-include $(shell "$(DIR)/setup-make.py")
+# clone projects, write project settings .mk file and source it
+include $(shell "$(DIR)/setup-make.py" $(MAKECMDGOALS))
 
 # default target
 all:
@@ -9,8 +9,6 @@ all:
 # main targets
 all: build
 
-checkout: $(patsubst %,%-checkout,$(PROJECTS))
-	@echo "checkout completed"
 use-git-https:
 	@$(MAKE) set-git-remote-url GIT_BASE=https://gitlab.cern.ch
 use-git-ssh:
@@ -38,46 +36,23 @@ help:
 	@for t in $(ALL_TARGETS) ; do echo .. $$t ; done
 
 # public targets: main targets
-ALL_TARGETS = all build checkout clean purge use-git-https use-git-ssh use-git-krb5 contrib
+ALL_TARGETS = all build clean purge use-git-https use-git-ssh use-git-krb5 contrib
 
 # ----------------------
 # implementation details
 # ----------------------
 
-# public targets: data package targets
-ALL_TARGETS += $(foreach p,$(DATA_PACKAGES),$(p) $(p)-checkout)
-
-define PACKAGE_settings
-$(1)-checkout:
-# put all packages in DBASE, even those in "PARAM"
-	@test -e DBASE/$(1) || ( \
-		mkdir -p DBASE && cd DBASE && \
-		/cvmfs/lhcb.cern.ch/lib/var/lib/LbEnv/644/stable/x86_64-centos7/bin/git-lb-clone-pkg $(1) \
-	)
-$(1): $(1)-checkout
-endef
-$(foreach p,$(DATA_PACKAGES),$(eval $(call PACKAGE_settings,$(p))))
-
-
 # public targets: project targets
-ALL_TARGETS += $(foreach p,$(PROJECTS),$(p) $(p)-checkout $(p)-clean $(p)-purge fast/$(p) fast/$(p)-clean)
+ALL_TARGETS += $(foreach p,$(PROJECTS),$(p) $(p)-clean $(p)-purge fast/$(p) fast/$(p)-clean)
 
 define PROJECT_settings
-# checkout
-$(1)-checkout:
-	@test -e $(1) || ( \
-		git clone $$($(1)_URL) $(1) && \
-		cd $(1) && \
-		git checkout $$($(1)_BRANCH) && \
-		git submodule update --init --recursive \
-	)
-# TODO the following is executed every time because $(1)-checkout is phony
-$(1)/run: $(1)-checkout $(DIR)/project-run.sh
+$(1)/run: $(DIR)/project-run.sh
+	@echo $(1)/run
 	@ln -sf $(DIR)/project-run.sh $(1)/run
 	@grep -Fxq "run" $(1)/.git/info/exclude || echo "run" >> $(1)/.git/info/exclude
 # generic build target
-$(1)/%: $(DATA_PACKAGES) $$($(1)_DEPS) fast/$(1)/% ;
-fast/$(1)/%: $(1)-checkout $(1)/run $(CONTRIB_DEPS)
+$(1)/%: $$($(1)_DEPS) fast/$(1)/% ;
+fast/$(1)/%: $(1)/run $(CONTRIB_DEPS)
 	@$(DIR)/build-env $(DIR)/make.sh $(1) $$*
 # exception for purge and clean: always do fast/Project/purge or clean
 $(1)/purge: fast/$(1)/purge ;
--- setup-make.py
+++ setup-make.py
@@ -3,10 +3,10 @@
 from __future__ import print_function
 import os
 import re
+import sys
 from config import read_config
 
 config = read_config()
-
 
 try:
     import pathlib
@@ -14,6 +14,7 @@ try:
     def mkdir_p(path):
         pathlib.Path(path).mkdir(parents=True, exist_ok=True)
 except ImportError:
+
     def mkdir_p(path):
         try:
             os.makedirs(path)
@@ -33,59 +34,145 @@ def parse_src_spec(spec):
     if not m:
         raise ValueError("malformed source spec '{}'".format(spec))
     g = m.groupdict()
-    return (
-        g['name'],
-        '{}/{}.git'.format(config['gitBase'], g['fullname']),
-        g["branch"] or config["defaultBranch"]
-        )
+    return (g['name'], '{}/{}.git'.format(config['gitBase'], g['fullname']),
+            g["branch"] or config['gitBranch']['default'])
 
 
-# save the host environment where we're executed
-output_path = config['outputPath']
-mkdir_p(output_path)
-with open(os.path.join(output_path, 'host.env'), 'w') as f:
-    for name, value in sorted(os.environ.items()):
-        print(name + "=" + value, file=f)
+def git_url_branch(project):
+    group = config['gitGroup']
+    branch = config['gitBranch']
+    return ('{}/{}/{}.git'.format(config['gitBase'],
+                                  group.get(project, group['default']),
+                                  project),
+            branch.get(project, branch['default']))
 
-projects = []
-urls = {}
-branches = {}
-for spec in config["projects"]:
-    p, url, branch = parse_src_spec(spec)
-    projects.append(p)
-    urls[p] = url
-    branches[p] = branch
 
-# Second pass once all projects are known to determine dependencies
-dependencies = {
-    p: set(config['projectDeps'].get(p, "").split(" ")).intersection(projects)
-    for p in projects
-}
-inv_dependencies = {
-    d: set(p for p in projects if d in dependencies[p])
-    for d in projects
-}
+def cmake_name(project):
+    with open(os.path.join(project, 'CMakeLists.txt')) as f:
+        cmake = f.read()
+    m = re.search(r'gaudi_project\(\s*(\w+)\s', cmake)
+    return m.group(1)
 
-data_packages = []
-for spec in config["dataPackages"]:
-    p, url, branch = parse_src_spec(spec)
-    data_packages.append(p)
-    urls[p] = url
-    branches[p] = branch
 
-config_path = os.path.join(output_path, "configuration.mk")
-with open(config_path, "w") as f:
-    print("CONTRIB_PATH := " + config["contribPath"], file=f)
-    print("PROJECTS := " + " ".join(projects), file=f)
+def cmake_deps(project):
+    with open(os.path.join(project, 'CMakeLists.txt')) as f:
+        cmake = f.read()
+    m = re.search(r'gaudi_project\(([^\)]+)\)', cmake)
+    return re.findall(r'\sUSE\s+(\w+)\s', m.group(1))
+
+
+def clone(project):
+    """Clone project and return canonical name."""
+    if not os.path.isdir(project):
+        m = [x for x in os.listdir('.') if x.lower() == project.lower()]
+        assert len(m) <= 1, 'Multiple directories for project: ' + str(m)
+        if not m:
+            url, branch = git_url_branch(project)
+            from subprocess import check_output
+            check_output(['git', 'clone', url])
+            check_output(['git', 'checkout', branch], cwd=project)
+            check_output(
+                ['git', 'submodule', 'update', '--init', '--recursive'],
+                cwd=project)
+            # TODO send output to stderr or log
+        else:
+            project = m[0]
+        canonical_name = cmake_name(project)
+        if canonical_name != project:
+            if not m:
+                os.rename(project, canonical_name)
+                project = canonical_name
+            else:
+                raise RuntimeError('Project {} already cloned under '
+                                   'non-canonical name {}'.format(
+                                       canonical_name, project))
+    return project
+
+
+def clone_package(name, path):
+    if not os.path.isdir(os.path.join(path, name)):
+        from subprocess import check_output
+        # TODO fix hardcoded LbEnv version
+        check_output([
+            '/cvmfs/lhcb.cern.ch/lib/var/lib/LbEnv/stable/x86_64-centos7/bin/git-lb-clone-pkg',
+            name
+        ],
+                     cwd=path)
+        # TODO send output to stderr or log
+
+
+def checkout(projects, data_packages):
+    """Clone projects and data packagas, and return make configuration.
+
+    The project dependencies of `projects` are cloned recursively.
+
+    """
+    to_checkout = list(projects)
+    projects = []
+    project_deps = {}
+
+    while to_checkout:
+        p = clone(to_checkout.pop(0))
+        deps = cmake_deps(p)
+        to_checkout.extend(sorted(set(deps).difference(projects)))
+        projects.append(p)
+        project_deps[p] = deps
+
+    # Second pass once all projects are known to determine dependencies
+    dependencies = {
+        p: set(project_deps[p]).intersection(projects)
+        for p in projects
+    }
+    inv_dependencies = {
+        d: set(p for p in projects if d in dependencies[p])
+        for d in projects
+    }
+
+    mkdir_p("DBASE")
+    for name in data_packages:
+        clone_package(name, "DBASE")
+
+    makefile_config = [
+        "PROJECTS := " + " ".join(projects),
+        "DATA_PACKAGES := " + " ".join(data_packages),
+    ]
     for p in projects:
-        print("{}_URL := {}".format(p, urls[p]), file=f)
-        print("{}_BRANCH := {}".format(p, branches[p]), file=f)
-        print("{}_DEPS := {}".format(p, ' '.join(dependencies[p])), file=f)
-        print("{}_INV_DEPS := {}".format(p, ' '.join(inv_dependencies[p])),
-              file=f)
-    print("DATA_PACKAGES := " + " ".join(data_packages), file=f)
-    for p in data_packages:
-        print("{}_URL := {}".format(p, urls[p]), file=f)
-        print("{}_BRANCH := {}".format(p, branches[p]), file=f)
+        makefile_config += [
+            "{}_DEPS := {}".format(p, ' '.join(dependencies[p])),
+            "{}_INV_DEPS := {}".format(p, ' '.join(inv_dependencies[p])),
+        ]
 
-print(config_path)
+    return makefile_config
+
+
+def main():
+    # save the host environment where we're executed
+    output_path = config['outputPath']
+    mkdir_p(output_path)
+    with open(os.path.join(output_path, 'host.env'), 'w') as f:
+        for name, value in sorted(os.environ.items()):
+            print(name + "=" + value, file=f)
+
+    projects = []
+    for arg in sys.argv[1:]:
+        m = re.match(r'(fast/)?(?P<project>[A-Z]\w+)(/.+)?', arg)
+        if m:
+            projects.append(m.group('project'))
+    projects = projects or [config['defaultProject']]
+
+    try:
+        makefile_config = checkout(projects, config['dataPackages'])
+        makefile_config += ["CONTRIB_PATH := " + config["contribPath"]]
+    except Exception as e:
+        print(str(e), file=sys.stderr)
+        makefile_config = ['$(error Error occurred in checkout)']
+
+    config_path = os.path.join(output_path, "configuration.mk")
+    with open(config_path, "w") as f:
+        f.write('\n'.join(makefile_config) + '\n')
+    # Print path so that the generated file can be included in one go
+    print(config_path)
+
+
+if __name__ == '__main__':
+    main()
