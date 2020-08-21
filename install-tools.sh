9f80889612014561ec5129a471c38b7d8e2f5a13
Rosen Matev <rosen.matev@cern.ch>
2020-08-21 13:14:08 +0200

    Commit a copy of external tools

--- install-tools.sh
+++ install-tools.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 set -eo pipefail
 
-. $(dirname $0)/install-common.sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-curl -o "${CONTRIB}/bin/post_build_ninja_summary.py" \
-    "https://cs.chromium.org/codesearch/f/chromium/tools/depot_tools/post_build_ninja_summary.py?cl=2d3b9260f3085f0ce161dbec51f131979b828474"
-chmod +x "${CONTRIB}/bin/post_build_ninja_summary.py"
+. "$DIR/install-common.sh"
 
-curl -o "${CONTRIB}/bin/ninjatracing" \
-    https://raw.githubusercontent.com/nico/ninjatracing/37e79ec0e570d08efaabfea10f3dcf8e4ad519f8/ninjatracing
-chmod +x "${CONTRIB}/bin/ninjatracing"
+cp "$DIR/external/post_build_ninja_summary.py" "${CONTRIB}/bin/"
+cp "$DIR/external/ninjatracing" "${CONTRIB}/bin/"
