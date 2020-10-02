74506470e3852ad82be3bdc38184f4dbea0f9678
Rosen Matev <rosen.matev@cern.ch>
2020-10-02 14:31:44 +0200

    Update to ccache 3.7.12

--- install-ccache.sh
+++ install-ccache.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 . $(dirname $0)/install-common.sh
 
 # master on 11 May 2019
-setup 'https://github.com/ccache/ccache.git' 'v3.7.7'
+setup 'https://github.com/ccache/ccache.git' 'v3.7.12'
 (
     set +e  # disable immediate exit because of incompatibility with CentOS 8
     . /cvmfs/sft.cern.ch/lcg/views/LCG_96b/x86_64-centos7-gcc9-opt/setup.sh
