4d2d3fe52df02c9d2be3b1e23e6a0792c8a3165d
Rosen Matev <rosen.matev@cern.ch>
2021-04-12 00:33:17 +0200

    Update LbEnv to latest stable version (2003)

--- native-env.sh
+++ native-env.sh
@@ -8,7 +8,8 @@ for i in /cvmfs/lhcb.cern.ch/lib/etc/cern_profile.d/*.sh ; do
 done
 
 lbenvPath=$1
-. "$lbenvPath"/bin/LbEnv.sh --quiet --siteroot /cvmfs/lhcb.cern.ch/lib
+. "$lbenvPath/bin/activate"
+eval $(python -m LbEnv --sh --quiet --siteroot /cvmfs/lhcb.cern.ch/lib)
 
 # print the entire environment
 env -0 | sort -z | xargs -0 bash -c 'printf "export %q\n" "$@"' _arg0
