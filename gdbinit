697f60e2c3fcd2974f91b2490de4901063e94a74
Rosen Matev <rosen.matev@cern.ch>
2021-06-15 23:02:34 +0200

    VSCode: Add GDB debugging configuration

---
+++ gdbinit
@@ -0,0 +1,1 @@
+set auto-load safe-path /cvmfs/lhcb.cern.ch/lib/lcg/releases
---
+++ project-gdb.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+self=$(target=$0 perl -le 'print readlink $ENV{target}')
+exec $(dirname $self)/run-env $(dirname $0) gdb "$@"
