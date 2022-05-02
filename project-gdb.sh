b6ddefd01e3d7109704b3c6405b600d4f587411b
Rosen Matev <rosen.matev@cern.ch>
2022-05-02 17:42:28 +0200

    Fix loading of gdbinit

--- project-gdb.sh
+++ project-gdb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 self=$(target=$0 perl -le 'print readlink $ENV{target}')
+utils=$(dirname $self)
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-# assume that utils ($DIR) is under $LBENV_CURRENT_WORKSPACE/$projectPath
 projectPath=$(dirname $DIR)
-exec $(dirname $self)/run-env $(basename "$DIR") \
-    gdb -iex "directory ${projectPath}" -ix "$DIR/gdbinit" "$@"
+exec $utils/run-env $(basename "$DIR") \
+    gdb -iex "directory ${projectPath}" -ix "$utils/gdbinit" "$@"
